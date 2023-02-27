56a64b735a753c5d796b6f83fdbd0604c589fa26
Pierrick Guillaume <pguillaume@sequans.com>
2023-02-27 13:20:52 +0100

    add: wait_start and read_addr

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use gpio_cdev::{self, Chip, EventRequestFlags, LineRequestFlags};
+use gpio_cdev::*;
 use quicli::prelude::*;
 use structopt::StructOpt;
+
+const I2C_CONSUMER: &str = "i2c-gpio-sqn";
 
 #[derive(Debug, StructOpt)]
 struct Cli {
@@ -12,6 +14,84 @@ struct Cli {
     scl: u32,
 }
 
+enum I2COp {
+    Read(u8),
+    Write(u8),
+}
+
+// Only addresses on 7 bits are supported
+fn read_addr(scl: &Line, sda: &Line) -> Result<I2COp, gpio_cdev::Error> {
+    let mut addr: u8 = 0;
+
+    let sda_handle = sda.request(LineRequestFlags::INPUT, 0, I2C_CONSUMER)?;
+    for (nr, _event) in scl
+        .events(
+            LineRequestFlags::INPUT,
+            EventRequestFlags::RISING_EDGE,
+            I2C_CONSUMER,
+        )?
+        .enumerate()
+    {
+        return match nr {
+            0..=6 => {
+                addr |= sda_handle.get_value()? << nr;
+                continue;
+            }
+            7 => match sda_handle.get_value()? {
+                1 => Ok(I2COp::Write(addr)),
+                _ => Ok(I2COp::Read(addr)),
+            },
+            _ => panic!("read address overflow"),
+        };
+    }
+
+    Ok(I2COp::Read(addr))
+}
+
+fn wait_start(scl: &Line, sda: &Line) -> Result<(), gpio_cdev::Error> {
+    let scl_handle = scl.request(LineRequestFlags::INPUT, 0, I2C_CONSUMER)?;
+
+    for _event in sda.events(
+        LineRequestFlags::INPUT,
+        EventRequestFlags::FALLING_EDGE,
+        I2C_CONSUMER,
+    )? {
+        return match scl_handle.get_value() {
+            Ok(1) => Ok(()),
+            _ => continue,
+        };
+    }
+
+    Ok(())
+}
+
+fn ack(scl: &Line, sda: &Line) -> Result<(), gpio_cdev::Error> {
+    // Request the sda line to low
+    sda.request(LineRequestFlags::OUTPUT, 0, I2C_CONSUMER)?;
+
+    // Wait for the next clock edge
+    scl.events(
+        LineRequestFlags::INPUT,
+        EventRequestFlags::RISING_EDGE,
+        I2C_CONSUMER,
+    )?
+    .next()
+    .unwrap()?;
+
+    Ok(())
+}
+
+fn nack(scl: &Line) -> Result<(), gpio_cdev::Error> {
+    // Just wait for the next clock edge, leaving sda to high
+    scl.events(
+        LineRequestFlags::INPUT,
+        EventRequestFlags::RISING_EDGE,
+        I2C_CONSUMER,
+    )?.next().unwrap()?;
+
+    Ok(())
+}
+
 fn do_main(args: Cli) -> Result<(), gpio_cdev::Error> {
     println!("i2c-gpio-sqn");
     let mut chip = Chip::new(args.chip)?;
@@ -19,15 +99,40 @@ fn do_main(args: Cli) -> Result<(), gpio_cdev::Error> {
     let scl = chip.get_line(args.scl)?;
     println!("chip: {:?}, sda: {:?}, scl: {:?}", chip, sda, scl);
 
-    for event in scl.events(
-        LineRequestFlags::INPUT,
-        EventRequestFlags::RISING_EDGE,
-        "i2c-gpio-sqn",
-    )? {
-        println!("clk edge rising ({:?})", event);
+    // Message loop
+    loop {
+        println!("Waiting for start condition...");
+        wait_start(&scl, &sda)?;
+        println!("Starting transaction");
+        match read_addr(&scl, &sda)? {
+            I2COp::Read(addr) => {
+                println!("Detected reading at address {addr}");
+                ack(&scl, &sda)?;
+                println!("acked address");
+                warn!("Reading is not implemented yet");
+                nack(&scl)?;
+                println!("nacked message");
+            }
+            I2COp::Write(addr) => {
+                println!("Detected writting at address {addr}");
+                ack(&scl, &sda)?;
+                println!("acked address");
+                warn!("Writting is not implemented yet");
+                nack(&scl)?;
+                println!("nacked message");
+            }
+        }
     }
 
-    Ok(())
+    // for event in scl.events(
+    //     LineRequestFlags::INPUT,
+    //     EventRequestFlags::RISING_EDGE,
+    //     "i2c-gpio-sqn",
+    // )? {
+    //     println!("clk edge rising ({:?})", event);
+    // }
+
+    // Ok(())
 }
 
 fn main() -> CliResult {
